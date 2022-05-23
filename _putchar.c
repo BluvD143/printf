d13ce03a69677381e860e4e2e750055d76efbb13
BELLO ABAYOMI <101479751+BluvD143@users.noreply.github.com>
2022-05-23 17:54:45 +0100

    _putchar.c file

--- _putchar.c
+++ _putchar.c
@@ -4,11 +4,25 @@
 /**
  * _putchar - to print a character
  * @c: character to be printed
+ * Description - this uses a local buffer of 1024 chars
  *
  * Return: written character
  */
 
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	static char buff[1024];
+	static int i;
+
+	if (c == -1 || i >= 1024)
+	{
+		write(1, &buff, i);
+		i = 0;
+	}
+	if (c != -1)
+	{
+		buff[i] = c;
+		i++;
+	}
+	return (1);
 }
